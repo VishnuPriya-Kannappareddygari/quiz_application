865e2181a23ad5d9e66492156aa16506d93e839d
VishnuPriya <Kannappareddygaari.VishnuPriya@gmail.com>
2025-07-29 23:54:12 +0530

    Initial Commit Of Quiz Application

---
+++ src/main/java/com/vishnu/quizapp/model/Question.java
@@ -0,0 +1,25 @@
+package com.vishnu.quizapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Question
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String category;
+    private String difficulty;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+}
